c0b802dd08d3dc1d9a1f3862c9c8aee779b401b9
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-08 16:08:02 +0000

    Agregar rutas de combos productos y reportes

--- app/Providers/PeluqueriaAppServiceProvider.php
+++ app/Providers/PeluqueriaAppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Http\Controllers\AplicacionController;
 use App\Http\Controllers\ClientPeluqueriaController;
+use App\Http\Controllers\ClientPeluqueriaExtrasController;
+use App\Http\Controllers\PeluqueriaExtrasController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,6 +29,19 @@ class PeluqueriaAppServiceProvider extends ServiceProvider
                 Route::put('servicios/{id}', [ClientPeluqueriaController::class,'actualizarServicio']);
                 Route::delete('servicios/{id}', [ClientPeluqueriaController::class,'eliminarServicio']);
 
+                Route::get('combos', [ClientPeluqueriaExtrasController::class,'combos']);
+                Route::post('combos', [ClientPeluqueriaExtrasController::class,'guardarCombo']);
+                Route::put('combos/{id}', [ClientPeluqueriaExtrasController::class,'actualizarCombo']);
+                Route::delete('combos/{id}', [ClientPeluqueriaExtrasController::class,'eliminarCombo']);
+
+                Route::get('productos', [ClientPeluqueriaExtrasController::class,'productos']);
+                Route::post('productos', [ClientPeluqueriaExtrasController::class,'guardarProducto']);
+                Route::put('productos/{id}', [ClientPeluqueriaExtrasController::class,'actualizarProducto']);
+                Route::delete('productos/{id}', [ClientPeluqueriaExtrasController::class,'eliminarProducto']);
+                Route::get('productos-movimientos', [ClientPeluqueriaExtrasController::class,'movimientos']);
+                Route::post('productos-movimientos', [ClientPeluqueriaExtrasController::class,'registrarMovimiento']);
+                Route::get('reportes', [ClientPeluqueriaExtrasController::class,'reportes']);
+
                 Route::get('personal', [ClientPeluqueriaController::class,'personal']);
                 Route::post('personal', [ClientPeluqueriaController::class,'guardarPersonal']);
                 Route::put('personal/{id}', [ClientPeluqueriaController::class,'actualizarPersonal']);
@@ -45,10 +60,24 @@ class PeluqueriaAppServiceProvider extends ServiceProvider
             });
 
         Route::middleware(['api','auth:sanctum','throttle:api','superadmin'])
-            ->prefix('api/v1/aplicaciones')
+            ->prefix('api/v1')
             ->group(function (): void {
-                Route::post('{aplicacion}/entregar', [AplicacionController::class,'entregar']);
-                Route::post('{aplicacion}/revocar-acceso', [AplicacionController::class,'revocar']);
+                Route::post('aplicaciones/{aplicacion}/entregar', [AplicacionController::class,'entregar']);
+                Route::post('aplicaciones/{aplicacion}/revocar-acceso', [AplicacionController::class,'revocar']);
+
+                Route::prefix('apps/peluqueria')->group(function (): void {
+                    Route::get('combos', [PeluqueriaExtrasController::class,'combos']);
+                    Route::post('combos', [PeluqueriaExtrasController::class,'guardarCombo']);
+                    Route::put('combos/{id}', [PeluqueriaExtrasController::class,'actualizarCombo']);
+                    Route::delete('combos/{id}', [PeluqueriaExtrasController::class,'eliminarCombo']);
+                    Route::get('productos', [PeluqueriaExtrasController::class,'productos']);
+                    Route::post('productos', [PeluqueriaExtrasController::class,'guardarProducto']);
+                    Route::put('productos/{id}', [PeluqueriaExtrasController::class,'actualizarProducto']);
+                    Route::delete('productos/{id}', [PeluqueriaExtrasController::class,'eliminarProducto']);
+                    Route::get('productos-movimientos', [PeluqueriaExtrasController::class,'movimientos']);
+                    Route::post('productos-movimientos', [PeluqueriaExtrasController::class,'registrarMovimiento']);
+                    Route::get('reportes', [PeluqueriaExtrasController::class,'reportes']);
+                });
             });
     }
 }
