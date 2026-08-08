11eda27d12661ca63e4c9bbcaf105ef07eb0e932
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-08 15:20:06 +0000

    Extender estado de entrega de aplicaciones

--- app/Models/Aplicacion.php
+++ app/Models/Aplicacion.php
@@ -6,8 +6,8 @@ class Aplicacion extends Model
 {
     use SoftDeletes;
     protected $table='aplicaciones';
-    protected $fillable=['empresa_id','proyecto_id','nombre','slug','version','tipo','tecnologias','entorno','estado','url','url_administracion','repositorio_url','proveedor_hosting','notas','publicado_at'];
-    protected $casts=['publicado_at'=>'datetime'];
+    protected $fillable=['empresa_id','proyecto_id','nombre','slug','version','tipo','tecnologias','entorno','estado','acceso_cliente','entregado_at','url','url_administracion','repositorio_url','proveedor_hosting','notas','publicado_at'];
+    protected $casts=['publicado_at'=>'datetime','entregado_at'=>'datetime','acceso_cliente'=>'boolean'];
     public function empresa(){return $this->belongsTo(Empresa::class);}
     public function proyecto(){return $this->belongsTo(Proyecto::class);}
     public function mantenimientos(){return $this->hasMany(Mantenimiento::class);}
