5ea2d98b12d676685332c726b1d57fd2b506a276
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-14 13:23:23 +0000

    Exponer panel operativo de órdenes Electrofrío

--- routes/electrofrio-extras.php
+++ routes/electrofrio-extras.php
@@ -1,18 +1,23 @@
 <?php
 
-use App\Http\Controllers\ElectrofrioFichaTecnicaController;
+use App\Http\Controllers\{ElectrofrioFichaTecnicaController,ElectrofrioOrdenOperativaController};
 use Illuminate\Support\Facades\Route;
 
-$technicalSheetRoutes = function (): void {
+$electrofrioExtraRoutes = function (): void {
     Route::get('fichas-tecnicas', [ElectrofrioFichaTecnicaController::class, 'index']);
     Route::get('equipos/{equipoId}/ficha-tecnica', [ElectrofrioFichaTecnicaController::class, 'show'])->whereNumber('equipoId');
     Route::put('equipos/{equipoId}/ficha-tecnica', [ElectrofrioFichaTecnicaController::class, 'guardar'])->whereNumber('equipoId');
+
+    Route::get('ordenes-operativas', [ElectrofrioOrdenOperativaController::class, 'index']);
+    Route::post('ordenes-operativas', [ElectrofrioOrdenOperativaController::class, 'guardar']);
+    Route::get('ordenes-operativas/{id}', [ElectrofrioOrdenOperativaController::class, 'show'])->whereNumber('id');
+    Route::put('ordenes-operativas/{id}', [ElectrofrioOrdenOperativaController::class, 'actualizar'])->whereNumber('id');
 };
 
 Route::middleware(['auth:sanctum', 'throttle:api', 'platform_admin'])
     ->prefix('apps/electrofrio')
-    ->group($technicalSheetRoutes);
+    ->group($electrofrioExtraRoutes);
 
 Route::middleware(['auth:sanctum', 'throttle:api', 'cliente'])
     ->prefix('mi/apps/electrofrio')
-    ->group($technicalSheetRoutes);
+    ->group($electrofrioExtraRoutes);
