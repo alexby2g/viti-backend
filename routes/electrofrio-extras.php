79cc1e58bf1c6146a331de16f76056ae9a47847c
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-17 02:42:27 +0000

    Exponer dashboard operativo del sistema de aires

--- routes/electrofrio-extras.php
+++ routes/electrofrio-extras.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\{ElectrofrioConfiguracionController,ElectrofrioDocumentacionController,ElectrofrioFichaTecnicaController,ElectrofrioHistorialEquipoController,ElectrofrioOrdenOperativaController,ElectrofrioPagoOperativoController};
+use App\Http\Controllers\{ElectrofrioConfiguracionController,ElectrofrioDashboardOperativoController,ElectrofrioDocumentacionController,ElectrofrioFichaTecnicaController,ElectrofrioHistorialEquipoController,ElectrofrioOrdenOperativaController,ElectrofrioPagoOperativoController};
 use Illuminate\Support\Facades\Route;
 
 $electrofrioExtraRoutes = function (): void {
     Route::get('configuracion', [ElectrofrioConfiguracionController::class, 'show']);
     Route::put('configuracion', [ElectrofrioConfiguracionController::class, 'update']);
+    Route::get('dashboard-operativo', ElectrofrioDashboardOperativoController::class);
 
     Route::get('fichas-tecnicas', [ElectrofrioFichaTecnicaController::class, 'index']);
     Route::get('equipos/{equipoId}/ficha-tecnica', [ElectrofrioFichaTecnicaController::class, 'show'])->whereNumber('equipoId');
