4c6927bb2c259ea4e8fa659f08248943f0f966f2
alexby2g <287088731+alexby2g@users.noreply.github.com>
2026-08-24 17:44:59 -0400

    feat: add project support cases

--- routes/support.php
+++ routes/support.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\{SupportChatController,SupportWorkspaceController};
+use App\Http\Controllers\{ProyectoSoporteController,SupportChatController,SupportWorkspaceController};
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum','throttle:api','soporte'])->prefix('soporte')->group(function (): void {
     Route::get('resumen', [SupportWorkspaceController::class,'overview']);
     Route::put('mantenimientos/{mantenimiento}/estado', [SupportWorkspaceController::class,'updateMaintenance']);
+
+    Route::get('proyectos/{proyecto}/casos', [ProyectoSoporteController::class,'index']);
+    Route::post('proyectos/{proyecto}/casos', [ProyectoSoporteController::class,'store']);
+    Route::put('proyectos/{proyecto}/casos/{mantenimiento}', [ProyectoSoporteController::class,'update']);
 
     Route::get('buzon', [SupportChatController::class,'index']);
     Route::get('buzon/{conversacion}', [SupportChatController::class,'show']);
